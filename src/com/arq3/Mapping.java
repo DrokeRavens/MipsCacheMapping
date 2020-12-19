1331229fc7de256f1b98d0cb6a5bbeb4bd0b282c
unknown <giovannymatheus28@gmail.com>
2020-12-18 22:47:11 -0300

    Mapeamento Associativo Direto

--- src/com/arq3/Mapping.java
+++ src/com/arq3/Mapping.java
@@ -68,5 +68,37 @@ public class Mapping {
 
         return result;
     }
+    
+    public static BlockResult CalculateAssociative(BlockInitial blockInitial) {
+        int cacheSize = blockInitial.mainInfo.bytesNaPalavra *
+                blockInitial.mainInfo.palavrasNoBloco *
+                blockInitial.mainInfo.conjuntoCache * 
+                blockInitial.mainInfo.viasNoConjunto;
+        //Contando que o valor de memorySize já esteja em bytes
+        int addrBitsCount = Long.toBinaryString(blockInitial.memorySize.value-1).length();
+        int bitsBlockPosition = Integer.toBinaryString((blockInitial.mainInfo.palavrasNoBloco*blockInitial.mainInfo.bytesNaPalavra)-1).length();
+        int bitsSet = 0;
+        int bitsTag = addrBitsCount-bitsBlockPosition-bitsSet;
+        BlockResult result = new BlockResult();
+        result.addrBitsCount = addrBitsCount;
+        result.cacheSize = cacheSize;
+        result.bitsBlockPosition = bitsBlockPosition;
+        result.bitsSet = bitsSet;
+        result.bitsTAG = bitsTag;
+        result.processoEnderecos = new BlockResult.ProcessoEnderecos();
+
+        for (int i = 0; i < blockInitial.acessosMemoria.length; i++) {
+                String baseString =
+                        "0".repeat(addrBitsCount - Integer.toBinaryString(blockInitial.acessosMemoria[i]).length())
+                        .concat(Integer.toBinaryString(blockInitial.acessosMemoria[i]));
+
+                int tag = Integer.parseInt(baseString.substring(0, bitsTag), 2);
+                int line = 0;
+                int block = Integer.parseInt(baseString.substring(bitsTag), 2);
+            result.processoEnderecos.linha.add(tag + " " + line + " " + block);
+        }
+
+        return result;
+    }
 
 }
